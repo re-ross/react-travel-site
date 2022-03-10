cd56c26af5231b989625c1d2919ff07ffef9236e
Ryan Ross <ryan@reross.com>
2022-03-09 20:43:03 -0600

    add routing to other pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import ".././App.css";
 import HeroSection from "../components/HeroSection";
 import React from "react";
 import Cards from "../components/Cards";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   );
 }
