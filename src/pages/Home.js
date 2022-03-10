3eaef6e96c634c91e13c92a277143278b53cb5b8
Ryan Ross <ryan@reross.com>
2022-03-09 19:52:47 -0600

    added styling and hero section

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import ".././App.css";
+import HeroSection from "../components/HeroSection";
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
