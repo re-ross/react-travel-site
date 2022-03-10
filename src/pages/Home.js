c388880527595e3cfc22819eb2139ba71be260eb
Ryan Ross <ryan@reross.com>
2022-03-09 20:10:50 -0600

    added cards and cardslist

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import ".././App.css";
 import HeroSection from "../components/HeroSection";
 import React from "react";
+import Cards from "../components/Cards";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 }
